519950d0a059bea2c16999bd5de4174b7918509e
Adrianna <adriannamoffitt06@gmail.com>
2023-11-09 13:13:52 -0600

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,47 @@
-
+import {puppyList} from "./data";
+import {useState} from "react";
 import './App.css'
 
 function App() {
-  
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+
+ const featuredPup = puppies.find((pup) => pup.id === featPupId);
+ console.log(featuredPup);
 
   return (
-    <>
-      <div>
+    <div className="App">
+      <div className="puppy-list">
+        <h1>Puppy List:</h1>
+        {puppies.map((puppy) => (
+          <p
+            onClick = {() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+        ))}
       </div>
-    
-    </>
-  )
+
+      {featPupId && (
+        <div className="puppy-id">
+          <h2>Puppy Info:</h2>
+          <h3>{featuredPup.name}</h3>
+          <ul>
+            <li>
+              <span>Age: {featuredPup.age} </span>
+            </li>
+            <li>
+              <span>Email: {featuredPup.email}</span>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
